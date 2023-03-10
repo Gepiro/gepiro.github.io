77ce18f1593677ea22a4c3532cba8abe3a0423a4
Gepiro <gepiro0916@gmail.com>
2023-03-10 18:28:59 +0100

    a

---
+++ src/Homepage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function HomePage() {
+    return (
+        <div>
+            <h1>Welcome to My Website!</h1>
+            <p>Thank you for visiting my website. Please feel free to browse and explore.</p>
+            <button>Get Started</button>
+        </div>
+    );
+}
+
+export default HomePage;
